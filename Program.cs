1fac9ee1b7711e9b0a006faed8e83250bd4fccad
Dildora Nasriddinzoda <80986802+BlackStar0020@users.noreply.github.com>
2021-08-09 14:52:50 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,14 +10,12 @@ namespace dz1
     {
        static void Main(string[] args)
          {
-             double a = 16.80;
+           /*  double a = 16.80;
              double b = 12.40;
              double result =Math.Round( Math.Sqrt(a*b), 2);
              Console.WriteLine($"result={result}");
-             Console.ReadKey();
-         }
-         static void Main(string[] args)
-          {
+             Console.ReadKey();*/
+         /*
               double A = 1.40;
               double B = -5.50;
               double C = 0.60;
@@ -27,36 +25,28 @@ namespace dz1
               Console.WriteLine($"dlinaAC={dlinaAC}");
               Console.WriteLine($"dlinaBC={dlinaBC}");
               Console.WriteLine($"summa={summa}");
-              Console.ReadKey();
-          }
-        static void Main(string[] args)
-        {
+              Console.ReadKey();*/
+         /*
             double X1 =-6.20, X2 = 2.10, Y1=5.20, Y2=9.80;
             double rastoyanie =Math.Round(( Math.Sqrt((Math.Pow((X2-X1), 2))+Math.Pow((Y2-Y1), 2))), 2);
             Console.WriteLine($"rastoyanie={rastoyanie}");
-            Console.ReadKey();
-       }
-       static void Main(string[] args)
-        {
+            Console.ReadKey();*/
+      /*
             
           
             Console.Write("Vvedite chislo:");
             int ab = Convert.ToInt32(Console.ReadLine());
             int ba = ab / 10 + ab % 10 * 10;
             Console.WriteLine($"Chislo pri perestanovke sifr:{ba}" );
-            Console.ReadKey();
-        }
-     static void Main(string[] args)
-        {
+            Console.ReadKey();*/
+/*
             int N = 10985;
             int minut = 60;
             int polnikhminut = N / minut;
             Console.WriteLine($"polnikhminut={polnikhminut}");
-            Console.ReadKey();
+            Console.ReadKey();*/
       
-        }
-      static void Main(string[] args)
-        {
+       
             int  odnanedelya = 7;
             Console.Write("Vvedite chislo:");
             int K = Convert.ToInt32(Console.ReadLine());
