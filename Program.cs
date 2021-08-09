1f68f5c72c68a988c49037269904d8e7064249a3
Dildora Nasriddinzoda <80986802+BlackStar0020@users.noreply.github.com>
2021-08-09 14:46:52 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,15 +8,15 @@ namespace dz1
 {
     class Program
     {
-      /*  static void Main(string[] args)
+       static void Main(string[] args)
          {
              double a = 16.80;
              double b = 12.40;
              double result =Math.Round( Math.Sqrt(a*b), 2);
              Console.WriteLine($"result={result}");
              Console.ReadKey();
-         }*/
-         /* static void Main(string[] args)
+         }
+         static void Main(string[] args)
           {
               double A = 1.40;
               double B = -5.50;
@@ -28,15 +28,15 @@ namespace dz1
               Console.WriteLine($"dlinaBC={dlinaBC}");
               Console.WriteLine($"summa={summa}");
               Console.ReadKey();
-          }*/
-       /* static void Main(string[] args)
+          }
+        static void Main(string[] args)
         {
             double X1 =-6.20, X2 = 2.10, Y1=5.20, Y2=9.80;
             double rastoyanie =Math.Round(( Math.Sqrt((Math.Pow((X2-X1), 2))+Math.Pow((Y2-Y1), 2))), 2);
             Console.WriteLine($"rastoyanie={rastoyanie}");
             Console.ReadKey();
-       }*/
-       /*static void Main(string[] args)
+       }
+       static void Main(string[] args)
         {
             
           
@@ -45,8 +45,8 @@ namespace dz1
             int ba = ab / 10 + ab % 10 * 10;
             Console.WriteLine($"Chislo pri perestanovke sifr:{ba}" );
             Console.ReadKey();
-        }*/
-     /*static void Main(string[] args)
+        }
+     static void Main(string[] args)
         {
             int N = 10985;
             int minut = 60;
@@ -54,8 +54,8 @@ namespace dz1
             Console.WriteLine($"polnikhminut={polnikhminut}");
             Console.ReadKey();
       
-        }*/
-     /*  static void Main(string[] args)
+        }
+      static void Main(string[] args)
         {
             int  odnanedelya = 7;
             Console.Write("Vvedite chislo:");
@@ -64,7 +64,7 @@ namespace dz1
             int dennedeli = K % odnanedelya;
             Console.WriteLine($"dennedeli:{dennedeli}");
             Console.ReadKey();
-        }*/
+        }
        
     }
 }
